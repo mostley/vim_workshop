b787104f366704e21e32508f60f57520806dad43
Sven Hecht <sven@understand.ai>
2022-03-13 14:57:11 +0100

    improve comparison and add why.txt

--- src/test.js
+++ src/test.js
@@ -1,20 +1,32 @@
 const fs = require("fs");
 
+const filenames = ["why", "x"];
 const usernamesData = fs.readFileSync("./src/usernames", {
   encoding: "utf8",
   flag: "r",
 });
 const usernames = usernamesData.split("\n").filter((a) => a);
-const filenames = ["x.txt"];
 
 describe("Exercises", () => {
-  describe.each(usernames)("%s", (username) => {
-    it.each(filenames)("%s", (filename) => {
-      const data = fs.readFileSync(`./src/users/${username}/${filename}`, {
+  let solutions = {};
+
+  beforeAll(() => {
+    for (const filename of filenames) {
+      const solutionData = fs.readFileSync(`./src/solutions/${filename}_solved.txt`, {
         encoding: "utf8",
         flag: "r",
       });
-      expect(data).toMatchSnapshot();
+      solutions[filename] = solutionData;
+    }
+  });
+
+  describe.each(usernames)("%s", (username) => {
+    it.each(filenames)("%s.txt", (filename) => {
+      const userExerciseData = fs.readFileSync(`./src/users/${username}/${filename}.txt`, {
+        encoding: "utf8",
+        flag: "r",
+      });
+      expect(userExerciseData).toEqual(solutions[filename]);
     });
   });
 });
